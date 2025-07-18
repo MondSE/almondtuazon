1d04ebd9204bcc5d5b2c638a7e3a657895e6dbc7
Almond Tuazon <azirtuazon@gmail.com>
2025-07-18 19:45:23 +0800

    header updated - json data inputed

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import * as motion from "motion/react-client";
+import headerData from "@/data/header.json";
 
 const Header = () => {
   return (
@@ -9,11 +10,13 @@ const Header = () => {
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.5 }}
     >
-      <div>
-        <h1 className="text-3xl font-bold">Almond Rae Tuazon</h1>
-        <p className="text-sm text-gray-400 mt-1">@ Pampanga, Philippines</p>
-        <p className="text-sm text-gray-400">Full-stack Engineer</p>
-      </div>
+      {headerData.map((hed, hedi) => (
+        <div key={hedi}>
+          <h1 className="text-3xl font-bold">{hed.name}</h1>
+          <p className="text-sm text-gray-400 mt-1">@ {hed.location}</p>
+          <p className="text-sm text-gray-400">{hed.Position}</p>
+        </div>
+      ))}
       <div className="flex flex-wrap gap-3">
         {/* <button className="bg-white text-black px-4 py-2 rounded text-sm font-medium hover:bg-gray-200 transition">
           Schedule a Call
